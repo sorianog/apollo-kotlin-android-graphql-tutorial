90f330fb378eba4b775b6cdc49ed7da29a159d7f
sorianog <13261711+sorianog@users.noreply.github.com>
2025-10-31 15:53:00 -0700

    Connect LaunchList to query

--- start/app/src/main/kotlin/com/example/rocketreserver/LaunchList.kt
+++ start/app/src/main/kotlin/com/example/rocketreserver/LaunchList.kt
@@ -2,6 +2,7 @@
 
 package com.example.rocketreserver
 
+import android.R.attr.onClick
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -10,11 +11,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -22,20 +29,25 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun LaunchList(onLaunchClick: (launchId: String) -> Unit) {
+    var launchList by remember { mutableStateOf(emptyList<LaunchListQuery.Launch>()) }
+    LaunchedEffect(Unit) {
+        val resp = apolloClient.query(LaunchListQuery()).execute()
+        launchList = resp.data?.launches?.launches?.filterNotNull() ?: emptyList()
+    }
     LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(20) {
-            LaunchItem(launchId = it.toString(), onClick = onLaunchClick)
+        items(launchList) { launch ->
+            LaunchItem(launch, onLaunchClick)
         }
     }
 }
 
 @Composable
-private fun LaunchItem(launchId: String, onClick: (launchId: String) -> Unit) {
+private fun LaunchItem(launch: LaunchListQuery.Launch, onClick: (launchId: String) -> Unit) {
     ListItem(
-        modifier = Modifier.clickable { onClick(launchId) },
+        modifier = Modifier.clickable { onClick(launch.id) },
         headlineContent = {
             // Mission name
-            Text(text = "Launch $launchId")
+            Text(text = "Launch ${launch.id}")
         },
         supportingContent = {
             // Site
