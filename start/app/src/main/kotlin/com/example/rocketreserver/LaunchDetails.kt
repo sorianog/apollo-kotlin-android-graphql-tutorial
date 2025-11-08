db77fb1a2ab5bc65618c138d9692f949ff05adfb
sorianog <13261711+sorianog@users.noreply.github.com>
2025-11-08 15:46:54 -0800

    Integrate query to details view

--- start/app/src/main/kotlin/com/example/rocketreserver/LaunchDetails.kt
+++ start/app/src/main/kotlin/com/example/rocketreserver/LaunchDetails.kt
@@ -1,6 +1,5 @@
 package com.example.rocketreserver
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.apollographql.apollo.api.ApolloResponse
 
 @Composable
@@ -32,9 +32,17 @@ fun LaunchDetails(launchId: String) {
     var resp by remember { mutableStateOf<ApolloResponse<LaunchDetailsQuery.Data>?>(null) }
     LaunchedEffect(Unit) {
         resp = apolloClient.query(LaunchDetailsQuery(launchId)).execute()
-        
     }
 
+    if (resp == null) {
+        Loading()
+    } else {
+        LaunchDetails(resp!!)
+    }
+}
+
+@Composable
+private fun LaunchDetails(resp: ApolloResponse<LaunchDetailsQuery.Data>) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -42,10 +50,12 @@ fun LaunchDetails(launchId: String) {
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             // Mission patch
-            Image(
+            AsyncImage(
                 modifier = Modifier.size(160.dp, 160.dp),
-                painter = painterResource(R.drawable.ic_placeholder),
-                contentDescription = "Mission patch"
+                model = resp?.data?.launch?.mission?.missionPatch,
+                placeholder = painterResource(R.drawable.ic_placeholder),
+                error = painterResource(R.drawable.ic_placeholder),
+                contentDescription = "Mission Patch"
             )
 
             Spacer(modifier = Modifier.size(16.dp))
@@ -54,21 +64,21 @@ fun LaunchDetails(launchId: String) {
                 // Mission name
                 Text(
                     style = MaterialTheme.typography.headlineMedium,
-                    text = "Launch $launchId"
+                    text = resp?.data?.launch?.mission?.name ?: "Mission N/A"
                 )
 
                 // Rocket name
                 Text(
                     modifier = Modifier.padding(top = 8.dp),
                     style = MaterialTheme.typography.headlineSmall,
-                    text = "Rocket name",
+                    text = resp?.data?.launch?.rocket?.name?.let { "🚀 $it" } ?: "Rocket N/A",
                 )
 
                 // Site
                 Text(
                     modifier = Modifier.padding(top = 8.dp),
                     style = MaterialTheme.typography.titleMedium,
-                    text = "Site..."
+                    text = resp?.data?.launch?.site ?: "Site N/A"
                 )
             }
         }
